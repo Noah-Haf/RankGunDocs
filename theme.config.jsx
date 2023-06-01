4accfb877bf2ebc0a9462db8138240439ff9d5de
Noah <61884850+Noah-Haf@users.noreply.github.com>
2023-06-01 22:10:42 +0100

    Update theme.config.jsx

--- theme.config.jsx
+++ theme.config.jsx
@@ -17,7 +17,7 @@ export default {
     <>
              <svg
         xmlns="http://www.w3.org/2000/svg"
-           width="24" height="24"
+           width="100" height="100"
         viewBox="0 0 456.8 98"
       >
         <path
